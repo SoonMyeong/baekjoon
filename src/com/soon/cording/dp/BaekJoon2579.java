1c35fc4da0be5dc8880c8c98b29ce8097d97a855
SoonMyeong_notebook <soon_myeong@naver.com>
2021-06-27 16:23:48 +0900

    계단 오르기(수정)

--- src/com/soon/cording/dp/BaekJoon2579.java
+++ src/com/soon/cording/dp/BaekJoon2579.java
@@ -21,19 +21,20 @@ public class BaekJoon2579 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int[] arr = new int[n];
-        int[] dp  = new int[n];
+        int[] arr = new int[n+1];
+        int[] dp  = new int[n+1];
 
-        for(int i=0; i<n; i++) {
+        for(int i=1; i<=n; i++) {
             arr[i] = scanner.nextInt();
         }
-        dp[0] = arr[0];
-        dp[1] = arr[0]+arr[1];
-        dp[2] = Math.max(arr[0]+arr[2],arr[1]+arr[2]);
-        for(int i=3; i<n; i++) {
-            dp[i]= Math.max(dp[i-3]+arr[i-1]+arr[i], dp[i-2]+arr[i]);
+        dp[1] = arr[1];
+        if(n>=2){
+            dp[2] = arr[1]+arr[2];
+        }
+        for (int i = 3; i <=n; i++) {
+            dp[i] = Math.max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i]);
         }
 
-        System.out.println(dp[n-1]);
+        System.out.println(dp[n]);
     }
 }
