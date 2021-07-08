9d7b19437583951a9f270dcb4d4c63a082b090ce
soonmyeong <smkwon@neonexsoft.com>
2021-07-08 23:19:53 +0900

    합분열 -> MOD 값 1억을 천만으로놔서 그랬음

--- src/com/soon/cording/dp/BaekJoon2225.java
+++ src/com/soon/cording/dp/BaekJoon2225.java
@@ -4,17 +4,15 @@ import java.util.Scanner;
 
 //합분해
 public class BaekJoon2225 {
-    private static final long NUMBER = 100000000;
+    private static final long NUMBER = 1000000000;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int k = scanner.nextInt();
         long[][] dp = new long[201][201];
 
-        for(int i=0; i<=n; i++) {
-            // 1개의 수로 i를 만들수 있는 경우의 수는 1개 뿐이다.
-            dp[0][i] = 0;
-            dp[1][i] = 1;
+        for(int i=1; i<=k; i++) {
+            dp[i][0] = 1;
         }
 
         /**
@@ -23,13 +21,11 @@ public class BaekJoon2225 {
          * 마지막 값 M을 더하기 전까지의 합은 N-M 이 된다.
          * 또한 개수는 K에서 M을 제외하기 때문에 -1 을 뺀 값이 된다.
          * 즉, k개의 수로 합 N을 만들어 내기 위한 DP 점화식은
-         *  dp[K][N] = = Σdp[K-1][N] 이 된다.?
+         *  dp[K][N] = = Σdp[K-1][N] 이 된다.
          */
-        for(int i=2; i<=k; i++) {
-            for(int j=0; j<=n; j++) {
-                for(int m=0; m<=j; m++) {
-                    dp[i][j] = (dp[i][j] + dp[i-1][j-m]) %NUMBER;
-                }
+        for(int i=1; i<=k; i++) {
+            for(int j=1; j<=n; j++) {
+                dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % NUMBER;
             }
         }
         System.out.println(dp[k][n]);
