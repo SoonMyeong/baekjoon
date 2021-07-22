d14451439ce6b55b4ea0b7067975617309cd33d2
soonmyeong <smkwon@neonexsoft.com>
2021-07-22 23:45:19 +0900

    DFS,BFS 구현 중...

--- src/com/soon/cording/그래프/BaekJoon1260.java
+++ src/com/soon/cording/그래프/BaekJoon1260.java
@@ -1,5 +1,28 @@
 package com.soon.cording.그래프;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * DFS & BFS 구현
+ * 오랜만에 다시 풀려니 기억 거의 안남... 다시 정리!
+ */
 public class BaekJoon1260 {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int v = scanner.nextInt();
+
+        int [][]arr = new int[n][n];
+        boolean[] visited = new boolean[n];
+
+
+        for(int i=0; i<m; i++) {
+
+        }
+
+    }
 }
