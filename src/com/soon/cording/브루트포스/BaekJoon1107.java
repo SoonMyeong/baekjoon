0ebe8255e2d99e5ad6a284a866f197111f72c4b1
soonmyeong <smkwon@neonexsoft.com>
2021-11-10 00:16:29 +0900

    [브루트포스] 리모콘.. 으 어렵다

---
+++ src/com/soon/cording/브루트포스/BaekJoon1107.java
@@ -0,0 +1,15 @@
+package com.soon.cording.브루트포스;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon1107 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int currentChannel = Integer.parseInt(br.readLine());
+
+
+    }
+}
