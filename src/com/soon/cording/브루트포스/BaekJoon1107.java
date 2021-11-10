174dfcf0b798dbe2015ddadf1af9be6880250edd
soonmyeong <smkwon@neonexsoft.com>
2021-11-11 00:10:11 +0900

    [브루트포스]

--- src/com/soon/cording/브루트포스/BaekJoon1107.java
+++ src/com/soon/cording/브루트포스/BaekJoon1107.java
@@ -9,6 +9,9 @@ public class BaekJoon1107 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int currentChannel = Integer.parseInt(br.readLine());
+        int brokenCount = Integer.parseInt(br.readLine());
+        String[] brokenButton = br.readLine().split(" ");
+
 
 
     }
